0bb74de5493477a610da7d2acd921b748c556f74
Chris Last <clast@qti.qualcomm.com>
2019-12-03 10:50:26 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,71 @@
+# pylint: disable=C0114, C0116, C0103
+from utils import get_input, IntcodeComputer
+
+DATA = get_input(3)
+
+
+def add(x, y, l, step):
+    dirn = step[0]
+    steps = int(step[1:])
+    for _ in range(steps):
+        if dirn == "U":
+            y += 1
+        elif dirn == "D":
+            y -= 1
+        if dirn == "L":
+            x -= 1
+        elif dirn == "R":
+            x += 1
+        l.add((x, y))
+    return x, y
+
+
+def part1():
+    """."""
+    wires = []
+    for wireplan in DATA:
+        wires.append(set())
+        x = y = 0
+        for step in wireplan.split(','):
+            x, y = add(x, y, wires[-1], step)
+    res = wires[0] & wires[1]
+    dist = [abs(x)+abs(y) for x, y in res]
+    return min(dist)
+
+
+def add2(x, y, l, m, n, step):
+    dirn = step[0]
+    steps = int(step[1:])
+    for _ in range(steps):
+        if dirn == "U":
+            y += 1
+        elif dirn == "D":
+            y -= 1
+        if dirn == "L":
+            x -= 1
+        elif dirn == "R":
+            x += 1
+        l.add((x, y))
+        n += 1
+        if ((x,y) not in m):
+            m[(x,y)] = n
+    return x, y, n
+
+
+def part2():
+    """."""
+    wires = []
+    stepcount = []
+    for wireplan in DATA:
+        wires.append(set())
+        stepcount.append(dict())
+        x = y = n = 0
+        for step in wireplan.split(','):
+            x, y, n = add2(x, y, wires[-1], stepcount[-1], n, step)
+    res = wires[0] & wires[1]
+    dist = [stepcount[0][(x,y)]+stepcount[1][(x,y)] for x, y in res]
+    return min(dist)
+
+
+print(f"\n    Part 1\n    {part1()}")
+print(f"\n    Part 2\n    {part2()}")
