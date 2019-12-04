7dcc729df2b67a790e773fac758dcf5baa14b50d
Chris Last <clast@qti.qualcomm.com>
2019-12-04 10:17:43 +0000

    day4

---
+++ day4.py
@@ -0,0 +1,38 @@
+# pylint: disable=C0114, C0116, W0611
+
+FIRST = 137683
+LAST = 596253
+
+
+def f1(n):
+    s = str(n)
+    if s[0]==s[1] or s[1]==s[2] or s[2]==s[3] or s[3]==s[4] or s[4]==s[5]:
+        if s[0]<=s[1]<=s[2]<=s[3]<=s[4]<=s[5]:
+            return True
+    return False
+
+
+def part1():
+    return len([x for x in range(FIRST, LAST+1) if f1(x)])
+
+
+def f3(s):
+    s = " " + s + " "
+    for i in range(1,6):
+        if s[i]==s[i+1] and s[i] != s[i-1] and s[i] != s[i+2]:
+            return True
+    return False
+
+def f2(n):
+    s = str(n)
+    if s[0]<=s[1]<=s[2]<=s[3]<=s[4]<=s[5] and f3(s):
+            return True
+    return False
+
+
+def part2():
+    return len([x for x in range(FIRST, LAST+1) if f2(x)])
+
+
+print(f"\n    Part 1\n    {part1()}")
+print(f"\n    Part 2\n    {part2()}")
