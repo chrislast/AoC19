ff5b5421420fb073b792c439b45ad8dee8319909
Chris Last <chris.last@gmail.com>
2021-09-26 14:06:43 +0100

    complete day 16 part 2

--- day16.py
+++ day16.py
@@ -33,7 +33,23 @@ def part1():
     return int(''.join([str(n) for n in res[:8]]))
 
 def part2():
-    pass
+    h = DATA[0]*10000
+    i = (h[int(h[0:7]):])
+    for a in range(100):
+        print(a)
+        string = ''
+        e = 0
+        while e < len(i):
+            if e == 0:
+                total = 0
+                for f in i:
+                    total += int(f)
+            elif e > 0:
+                total -= int(i[e-1])
+            string += str(total)[-1]
+            e+=1
+        i = string
+    return i[0:8]
 
 if __name__ == "__main__":
     print(f"\n    Part 1\n    {part1()}\n")
